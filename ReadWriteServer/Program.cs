a94b6d91a611ae86f5d5ea1bd3634dc9563107f4
Alex <foxandr98@mail.ru>
2023-08-23 13:56:11 +0500

    ReaderWriterLock. Добавлен общий ресурс и логика для работы в многопоточной среде.
    Блокировка выполняется с помощью объекта ReaderWriterLock, который
    позволяет синхронизировать доступ к общему ресурсу, но в единый момент
    времени позволяет дать параллельный доступ на чтение разным потокам или же
    доступ на запись лишь для одного потока

--- ReadWriteServer/Program.cs
+++ ReadWriteServer/Program.cs
@@ -1,14 +1,53 @@
-﻿
-namespace ReadWriteServer
+﻿namespace ReadWriteServer
 {
     public class Program
     {
+        private const int numThreads = 20;
+        private static bool running = true;
         public static void Main()
         {
-            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Запуск...");
 
-            Console.WriteLine("Hello, World!");
+            // Серия потоков, которые рандомно читают и пишут
+            // данные из общего разделяемого ресурса
+            Thread[] t = new Thread[numThreads];
+            for (int i = 0; i < numThreads; i++)
+            {
+                t[i] = new Thread(new ThreadStart(SomeAction));
+                // Именование потоков, начиная с "А"
+                t[i].Name = new String(new char[] { (char)(i + 65) });
+                Console.WriteLine(t[i].Name + " created");
+                t[i].Start();
+            }
 
+            // Ждем 10 секунд
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            // Завершаем потоки
+            running = false;
+            for (int i = 0; i < numThreads; i++)
+                t[i].Join();
+
+            //Печатаем статистику по действиям и таймаутам
+            Server.PrintStatistics();
+        }
+
+        public static void SomeAction()
+        {
+            Random rnd = new Random();
+            while (running)
+            {
+                int action = rnd.Next(0, 2);
+                switch (action)
+                {
+                    case 0:
+                        Server.GetCount();
+                        break;
+                    case 1:
+                        Server.AddToCount(rnd.Next(10));
+                        break;
+                }
+                Thread.Sleep(500);
+            }
         }
     }
 }
--- ReadWriteServer/Server.cs
+++ ReadWriteServer/Server.cs
@@ -1,26 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReadWriteServer
+﻿namespace ReadWriteServer
 {
     public static class Server
     {
+        // Блокировка
         static ReaderWriterLock rwl = new ReaderWriterLock();
 
-        //Shared resource
+        // Общий ресурс
         private static int count = 0;
+
+        // Статистика.
+        public static int readerTimeouts = 0;
+        public static int writerTimeouts = 0;
+        public static int reads = 0;
+        public static int writes = 0;
+
+        // Константа времени таймаута
+        private const int timeOut = 50;
 
         public static int GetCount()
         {
+            try
+            {
+                rwl.AcquireReaderLock(timeOut);
+                try
+                {
+                    Display(String.Format("Чтение ресурса count: {0}", count));
+                    Interlocked.Increment(ref reads);
+                }
+                finally
+                {
+                    rwl.ReleaseReaderLock();
+                }
+            }
+            catch (Exception ex)
+            {
+                Interlocked.Increment(ref readerTimeouts);
+            }
+
             return count;
         }
 
         public static void AddToCount(int add)
         {
+            try
+            {
+                rwl.AcquireWriterLock(timeOut);
+                try
+                {
+                    string msg = String.Format("Добавление числа к ресурсу count. {0} + {1} = ", add, count);
+                    count += add;
+                    msg += count;
+                    Display(msg);
+                    Interlocked.Increment(ref writes);
+                }
+                finally
+                {
+                    rwl.ReleaseWriterLock();
+                }
+            }
+            catch (Exception ex)
+            {
+                Interlocked.Increment(ref writerTimeouts);
+            }
+        }
 
+        private static void Display(string message)
+        {
+            Console.WriteLine(String.Format("Thread {0}: {1}", Thread.CurrentThread.Name, message));
+        }
+
+        public static void PrintStatistics()
+        {
+            // Отобразить общую статистику
+            string stats = @"Статистика:
+    {0} чтений 
+    {1} записей 
+    {2} таймаутов при чтении 
+    {3} таймаутов при записи";
+            Console.WriteLine(stats, reads, writes, readerTimeouts, writerTimeouts);
         }
     }
 }
