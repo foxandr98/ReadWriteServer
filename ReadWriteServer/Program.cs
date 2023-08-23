b5fb0bdacd64877b1548ee584634b730cad6ea92
Alex <foxandr98@mail.ru>
2023-08-23 12:10:30 +0500

    Инициализация, создание шаблона класса сервера, создание .gitignore

---
+++ ReadWriteServer/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ReadWriteServer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // See https://aka.ms/new-console-template for more information
+
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
---
+++ ReadWriteServer/Server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWriteServer
+{
+    public static class Server
+    {
+        static ReaderWriterLock rwl = new ReaderWriterLock();
+
+        //Shared resource
+        private static int count = 0;
+
+        public static int GetCount()
+        {
+            return count;
+        }
+
+        public static void AddToCount(int add)
+        {
+
+        }
+    }
+}
